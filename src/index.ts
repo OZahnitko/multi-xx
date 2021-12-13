63d9af47b9a832de7ac044acce1101c7aad1ae89
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-12-13 18:12:37 +1000

    did not delete everything

--- src/index.ts
+++ src/index.ts
@@ -2,71 +2,7 @@ import { existsSync, lstatSync, mkdirSync, readdirSync, unlinkSync } from "fs";
 
 import { resolve } from "path";
 
-import {
-  lstatPromise,
-  readDirPromise,
-  readFilePromise,
-  writeFilePromise,
-} from "./utils";
-
-// const readDirs = async () => {
-//   const observedRepos = (
-//     await readDirPromise(resolve(__dirname, "../.."))
-//   ).filter(
-//     (dir) => resolve(__dirname, "../..", dir) !== resolve(__dirname, "..")
-//   );
-//   console.log(observedRepos);
-// };
-
-// readDirs();
-
-// (async () => {
-//   const content = (await readDirPromise(resolve(__dirname, "../../multi-24")))
-//     .filter((dir) => !IGNORE_LIST.includes(dir))
-//     .map(async (dir) => {
-//       return (
-//         await lstatPromise(resolve(__dirname, "../../multi-24", dir))
-//       ).isDirectory();
-//     });
-//   console.log(content);
-// })();
-
-// (async () => {
-//   const f = await Promise.all(
-//     (
-//       await readDirPromise(resolve(__dirname, "../..", "multi-24"))
-//     ).filter((dir) => !IGNORE_LIST.includes(dir))
-//   );
-//   f.forEach(async (f) => {
-//     const isDir = (
-//       await lstatPromise(resolve(__dirname, "../../multi-24", f))
-//     ).isDirectory();
-//   });
-// })();
-
-// const files = <string[]>[];
-
-// const throughDir = async (parentDir: string) => {
-//   const dirContent = (await readDirPromise(parentDir))
-//     .filter((dir) => !IGNORE_LIST.includes(dir))
-//     .filter(
-//       (dir) => resolve(parentDir, dir) !== resolve(parentDir, "multi-xx")
-//     );
-
-//   dirContent.forEach(async (dir) => {
-//     const absPath = resolve(parentDir, dir);
-//     if (!(await lstatPromise(absPath)).isDirectory()) {
-//       return files.push(absPath);
-//     } else {
-//       return throughDir(absPath);
-//     }
-//   });
-// };
-
-// (async () => {
-//   const s = await throughDir(resolve(__dirname, "../.."));
-//   console.log(s);
-// })();
+import { readFilePromise, writeFilePromise } from "./utils";
 
 const IGNORE_LIST = [".git", ".terraform", "build", "multi-xx", "node_modules"];
 
@@ -93,7 +29,7 @@ const writeFiles = async (files: string[]) => {
     readmeFiles.map(async (file) => ({
       content: (await readFilePromise(file)).toString(),
       filePath: file,
-      name: file.split("/").slice(4).join("-"),
+      name: file.split("/").slice(4).join("."),
     }))
   );
 
@@ -104,15 +40,16 @@ const writeFiles = async (files: string[]) => {
   // Remove all old files from the "docs" directory.
   readdirSync(resolve(__dirname, "../docs")).forEach((file) => {
     console.log(resolve(__dirname, "../docs", file));
-    // unlinkSync(resolve(__dirname))
+    unlinkSync(resolve(__dirname, "../docs", file));
   });
 
-  // filesData.forEach(async (file) => {
-  //   await writeFilePromise(
-  //     resolve(__dirname, "../docs", file.name),
-  //     file.content
-  //   );
-  // });
+  // Write all the README.md files.
+  filesData.forEach(async (file) => {
+    await writeFilePromise(
+      resolve(__dirname, "../docs", file.name),
+      file.content
+    );
+  });
 };
 
 writeFiles(readmeFiles);
